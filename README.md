c647d37e5398c9b2c9edf21999a518698ed066b3
Cristian Bogdan <cbogdan.ca@gmail.com>
2024-07-24 16:59:23 -0400

    Merged in Material subsystem

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tuff Shader Loader
+# Tuff Shader Loader + Material subsystem
 Loader of custom shaders for the [Tuff game engine](https://github.com/Vespidian/GameFramework).
 
 ## SHADERS (.shader)
@@ -19,3 +19,16 @@ Requires both a fragment and vertex stage
 - range		(Allowable range for integer and floating point values)
 
 #### [Example Base Shader](assets/default.shader) - See also [all the test shaders](assets/)
+
+
+## MATERIALS (.mat):
+A material bundles together a shader and multiple textures / values to be assigned to said shader
+
+- Must have a base shader
+- May have 0 or more parameters
+
+Parameters types include:
+- texture 	(1D, 2D, 3D)
+- value 	(float, int, bool)
+
+Uniform value type (float, int, ..) and ranges (0-255, 0.0-1.0, ..) are specified in shader files
--- src/main.c
+++ src/main.c
@@ -6,10 +6,9 @@
 #include "debug.h"
 
 #include "shader.h"
+#include "material.h"
 
-int main(){
-	InitDebug();
-
+void TestShaderLib(){
 	// for(int i = 0; i < 20; i++){
 		Shader default_shader = ShaderOpen("../assets/default.shader");
 		Shader axis_shader = ShaderOpen("../assets/axis.shader");
@@ -38,6 +37,33 @@ int main(){
 		ShaderFree(&quad_shader);
 		ShaderFree(&ui_shader);
 	// }
+
+}
+
+void TestMaterialLib(){
+	Shader default_shader;
+	default_shader = ShaderOpen("../assets/default.shader");
+	Material default_material = MaterialOpen("../assets/default.mat");
+
+	printf("Tint.v[2] = %f\n", MaterialUniformFind(&default_material, "tint_u")->value._vec3.z);
+
+	MaterialShaderSet(&default_material, &default_shader);
+	MaterialUniformsValidate(&default_material);
+	MaterialShaderPassUniforms(&default_material);
+
+	for(int i = 0; i < 100; i++){
+		MaterialReload(&default_material);
+	}
+
+	MaterialFree(&default_material);
+	ShaderFree(&default_shader);
+}
+
+int main(){
+	InitDebug();
+
+	TestShaderLib();
+	TestMaterialLib();
 
 	DebugLog(D_ACT, "-- DONE-- ");
 	QuitDebug();
